bfed980169b076c552f3e6603eee864890cd935c
ViggaD <danhkvtse172932@gmail.com>
2024-05-24 11:55:08 +0700

    init new project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from "./Redux/Store"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
