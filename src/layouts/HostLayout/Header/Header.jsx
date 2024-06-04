13599fbd3e1f1a567cc5787efca84547105c8054
leehai1107 <leehai1107@gmail.com>
2024-06-04 08:38:19 +0700

    update:add header and modfied the footer at sider

--- src/layouts/HostLayout/Header/Header.jsx
+++ src/layouts/HostLayout/Header/Header.jsx
@@ -1,4 +1,11 @@
+import { Avatar, Space } from "antd";
 import { Header as HeaderAntd } from "antd/es/layout/layout";
+import {
+  DownOutlined,
+  UserOutlined,
+  SettingTwoTone,
+  QuestionCircleTwoTone,
+} from "@ant-design/icons";
 
 function Header() {
   return (
@@ -9,7 +16,15 @@ function Header() {
           background: "white",
         }}
       >
-        Header
+        <Space className="flex justify-end items-center pr-6">
+          <QuestionCircleTwoTone className="flex text-2xl p-1" />
+          <SettingTwoTone className="flex text-2xl p-1" />
+          <div className="flex items-center font-semibold ">
+            <Avatar size={42} className="ml-4 mr-5" icon={<UserOutlined />} />
+            Host
+            <DownOutlined className="text-xl ml-5" />
+          </div>
+        </Space>
       </HeaderAntd>
     </>
   );
