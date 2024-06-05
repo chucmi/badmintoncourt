97e83c878f78fefce36bcf1824170f8e95f91f36
leehai1107 <leehai1107@gmail.com>
2024-06-05 13:47:39 +0700

    update: add label for the form

--- src/components/CourtForm/CourtForm.jsx
+++ src/components/CourtForm/CourtForm.jsx
@@ -1,16 +1,23 @@
-import { Form, Input } from "antd";
+import { Button, Form, Input } from "antd";
 import FormInput from "../FormInput/FormInput";
 
 export default function CourtForm() {
   return (
     <>
       <Form>
+        <div className="font-bold text-2xl">Add Your Court</div>
         <FormInput
-          label={"Court Name"}
-          name={"courtName"}
+          label={"Court Name:"}
+          name={"Court Name"}
           rules={[{ required: true }]}
         >
           <Input />
+        </FormInput>
+
+        <FormInput>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
         </FormInput>
       </Form>
     </>
