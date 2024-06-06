a1d2d73a497efe3c592a4a72bbaeef11db4fdfe0
leehai1107 <leehai1107@gmail.com>
2024-06-06 13:53:33 +0700

    update: separate for the host layout to manager layout for reusable

--- src/pages/HostPage/HostPage.jsx
+++ src/pages/HostPage/HostPage.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { Layout } from "antd";
-import Header from "../../layouts/HostLayout/Header/Header";
-import HostSider from "../../layouts/HostLayout/Sider/HostSider";
+import Header from "../../layouts/ManagerLayout/Header/Header";
 import CourtForm from "../../components/CourtForm/CourtForm";
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UserOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
+import ManagerSider from "../../layouts/ManagerLayout/Sider/ManagerSider";
 
 const { Content } = Layout;
 
@@ -16,18 +24,47 @@ const labels = [
   "Logout",
 ];
 
+const items = [
+  BarChartOutlined,
+  AppstoreOutlined,
+  TeamOutlined,
+  UserOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: labels[index],
+}));
+
+// Add the logout item separately
+items.push({
+  key: String(items.length + 1),
+  icon: React.createElement(LogoutOutlined),
+  label: (
+    <a target="_blank" rel="Logout" href="/">
+      Logout
+    </a>
+  ),
+});
+
+const backgroundColor = "white";
+const themeBackgroundColor = "light";
+
 export default function HostPage() {
   return (
     <>
       <Layout hasSider>
-        <HostSider />
+        <ManagerSider
+          backgroundColor={backgroundColor}
+          themeBackgroundColor={themeBackgroundColor}
+          items={items}
+        />
 
         <Layout
           style={{
             marginLeft: width,
           }}
         >
-          <Header />
+          <Header username="Host" />
 
           <Content className="text-center h-screen flex text-black">
             <CourtForm />
