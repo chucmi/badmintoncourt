d85c96a9c5bc25b9febce703bd7a464a18350075
leehai1107 <leehai1107@gmail.com>
2024-06-24 21:12:30 +0700

    fix: localhost call

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -26,7 +26,7 @@ const Register = () => {
     e.preventDefault();
     console.log("submit");
     const response = await axios.post(
-      "http://localhost:8080/api/v1/auth/signup",
+      "https://badmintonbookingserver.up.railway.app/api/v1/auth/signup",
       {
         username,
         email,
