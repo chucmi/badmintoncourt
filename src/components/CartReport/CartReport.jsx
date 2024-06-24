18c8598962ddcfad198e247f349ed5f7013f4252
leehai1107 <leehai1107@gmail.com>
2024-06-24 08:56:23 +0700

    update: add order

--- src/components/CartReport/CartReport.jsx
+++ src/components/CartReport/CartReport.jsx
@@ -1,16 +1,45 @@
-import { Button, Card, Col, Divider, List, Row, Typography } from "antd";
-import { useDispatch, useSelector } from "react-redux";
+import { Button, Card, Divider, List, Typography, notification } from "antd";
+import { useSelector } from "react-redux";
+import { createBookingOrdersBulk } from "../../services/orderAPI";
+import { useNavigate } from "react-router-dom";
 const { Text } = Typography;
-
-const orderItems = [
-  { name: "Sân cầu lông...", price: "40.000 đ", quantity: 1 },
-  { name: "Sân cầu lông...", price: "40.000 đ", quantity: 1 },
-  { name: "Sân cầu lông...", price: "40.000 đ", quantity: 1 },
-];
 
 const totalAmount = "0.000 đ";
 export default function CartReport() {
   const cartItems = useSelector((state) => state.cart.cartItems);
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  let userId = null;
+  if (token) {
+    userId = JSON.parse(atob(token.split(".")[1])).id;
+  }
+
+  const handleCheckout = async () => {
+    if (cartItems.length === 0) {
+      notification.warning({
+        message: "Giỏ hàng trống",
+        description:
+          "Vui lòng thêm sản phẩm vào giỏ hàng trước khi thanh toán.",
+      });
+      return;
+    }
+
+    if (cartItems.length > 0 && token === null) {
+      navigate("/login");
+      return;
+    }
+
+    const orderItems = cartItems.map((item) => ({
+      yardId: item.id,
+      userId: userId,
+    }));
+
+    try {
+      await createBookingOrdersBulk(orderItems);
+    } catch (error) {
+      console.error("Error during checkout:", error);
+    }
+  };
 
   return (
     <Card
@@ -35,7 +64,12 @@ export default function CartReport() {
           {totalAmount}
         </Text>
       </div>
-      <Button type="primary" block style={{ marginTop: 16 }}>
+      <Button
+        type="primary"
+        block
+        style={{ marginTop: 16 }}
+        onClick={handleCheckout}
+      >
         Thanh toán
       </Button>
     </Card>
