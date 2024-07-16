39e1380a54e66d93a729cfa5995a115db445d237
leehai1107 <leehai1107@gmail.com>
2024-07-16 20:18:04 +0700

    update

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -11,7 +11,9 @@ export default function UserForm({ user, onFinish, form }) {
   };
 
   useEffect((user) => {
-    form.setFieldsValue({});
+    form.setFieldsValue({
+      gender: true,
+    });
   }),
     [user];
 
@@ -79,9 +81,7 @@ export default function UserForm({ user, onFinish, form }) {
             rules={[{ required: true, message: "Please select the gender" }]}
           >
             <select className="w-full h-9 border-2 rounded-lg">
-              <option value={true} selected>
-                Male
-              </option>
+              <option value={true}>Male</option>
               <option value={false}>Female</option>
               <option value={""}>Other</option>
             </select>
